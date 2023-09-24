1320f55bae1e49210b26f84e2237bebf0a7450db
PikiLee <871565198@qq.com>
2023-09-24 22:55:34 +0800

    test(template): find causes for the error

---
+++ src/template/error.ts
@@ -0,0 +1,44 @@
+import type { Template } from './type'
+
+const applyTemplate = (input: string) => {
+  return `Your task is to perform the following actions:
+  1 - Find plausible reasons for the error delimited by """.
+  2 - Come up with at least three possible reasons and explain why.
+  3 - For every reason, come up a solution and explain the solution.
+  4 - Show code example for every solution
+
+  Use the following format:
+  # Reasons
+  <Reasons>
+
+  # Reason1
+  <Reason1>
+  ## Explaination
+  <Explaination>
+  ## Solution
+  <Solution>
+  <Code Example>
+  ### Explaination fot the solution
+  <Explaination fot the solution>
+
+  # Reason2
+  <Reason2>
+  ## Explaination
+  <Explaination>
+  ## Solution
+  <Solution>
+  <Code Example>
+  ### Explaination fot the solution
+  <Explaination fot the solution>
+
+  """
+  ${input}
+  """
+`
+}
+
+export const error: Template = {
+  type: 'single',
+  title: 'Find causes for the error',
+  applyTemplate,
+}
--- src/template/index.ts
+++ src/template/index.ts
@@ -1,7 +1,9 @@
+import { error } from './error'
 import { ideas } from './ideas'
 import { tones } from './tone'
 
 export const templates = [
   tones,
   ideas,
+  error,
 ]
