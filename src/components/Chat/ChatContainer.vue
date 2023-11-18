53fb857acae9992872eb4ad784cc5ab0a8906116
PikiLee <871565198@qq.com>
2023-11-18 22:28:26 +0800

    fix: scrolling of wrapper element

--- src/components/Chat/ChatContainer.vue
+++ src/components/Chat/ChatContainer.vue
@@ -14,14 +14,25 @@ watch(error, (_error) => {
 
 // scroll
 const wrapperEl = ref<HTMLElement>()
+let keepInEnd = false
 const { y } = useScroll(wrapperEl, { behavior: 'smooth' })
+
+const _sendMessage = () => {
+  keepInEnd = true
+  sendMessage()
+}
+
 watch(() => messages, () => {
-  if (wrapperEl.value) {
-    setTimeout(() => {
-      y.value = wrapperEl.value!.scrollHeight
-    }, 100)
-  }
+  if (wrapperEl.value && keepInEnd)
+    y.value = wrapperEl.value!.scrollHeight
 }, { immediate: true, deep: true })
+
+onMounted(() => {
+  window.addEventListener('wheel', (event: WheelEvent) => {
+    if (event.deltaY < 0)
+      keepInEnd = false
+  })
+})
 </script>
 
 <template>
@@ -30,7 +41,7 @@ watch(() => messages, () => {
       <ChatMessageCard v-for="m, index in messages" :key="`${m.content?.slice(0, 10)}+++${index}`" :message="m" />
     </ul>
 
-    <MessageInput v-model="input" :loading="loading" @send="sendMessage" @clear="clearHistory" />
+    <MessageInput v-model="input" :loading="loading" @send="_sendMessage" @clear="clearHistory" />
   </section>
 </template>
 
