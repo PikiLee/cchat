ac7bd0f841ba8cf36d5b8b97047fe435fd52bf4a
PikiLee <871565198@qq.com>
2024-09-15 10:35:38 +0800

    feat: [openai] support gpt-4o and o1

--- src/stores/useSettingStore.ts
+++ src/stores/useSettingStore.ts
@@ -1,6 +1,7 @@
+import type { ChatModel } from 'openai/resources'
 import { defineStore } from 'pinia'
 
-export const models = [
+export const models: { value: ChatModel; label: string }[] = [
   {
     value: 'gpt-3.5-turbo',
     label: 'GPT-3.5 Turbo',
@@ -12,6 +13,22 @@ export const models = [
   {
     value: 'gpt-4-turbo',
     label: 'GPT-4 Turbo',
+  },
+  {
+    value: 'gpt-4o',
+    label: 'GPT-4o',
+  },
+  {
+    value: 'gpt-4o-mini',
+    label: 'GPT-4o-mini',
+  },
+  {
+    value: 'o1-mini',
+    label: 'o1-mini',
+  },
+  {
+    value: 'o1-preview',
+    label: 'o1-preview',
   },
 ]
 
